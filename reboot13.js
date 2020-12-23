f98514bd1076364d159c7d645f1224e743bccf48
Krutik <58563133+reboot13-git@users.noreply.github.com>
2020-12-23 23:37:57 +0530

    Add files via upload

---
+++ reboot13.js
@@ -0,0 +1,23 @@
+window.onload(){
+
+///Classroom Codes
+	let EmeCode = "";
+	let BeeCode = "";
+	let CsCode = "";
+	let WsCode = "";
+	let IctCode = "";
+	let MathCode= "";
+	let ActCode = "";
+	let EsCode = "";
+ ///Lecture Links
+    let EmeLink = "https://meet.google.com/lookup/dhngb2hxlr";
+	let BeeLink = "";
+	let CsLink = "https://meet.google.com/lookup/fxszdmcw3y";
+	let WsLink = "";
+	let IctLink = "";
+	let MathLink= "https://meet.google.com/lookup/fqte77ij25";
+	let ActLink = "";
+	let EsLink = "";
+
+document.getElementById('').innerHTML = ""
+}
